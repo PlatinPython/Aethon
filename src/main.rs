1ec34de8c308e49d25dbf624ae08386ff1d545bf
PlatinPython <robert.alexander.kaempf@gmail.com>
2023-11-02 02:00:26 +0100

    Create a custom launcher profile.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fs;
+
+use serde_json::{json, Value};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let path = dirs::config_dir()
+        .map(|path| path.join(".minecraft/launcher_profiles.json"))
+        .expect("Path should exist.");
+    let mut value = serde_json::from_str::<Value>(&fs::read_to_string(&path)?)?;
+    if !value["profiles"].is_object() {
+        value["profiles"] = json!({});
+    }
+    let profiles = &mut value["profiles"];
+    profiles["aethon"] = json!({
+        "name": "Aethon",
+        "type": "custom",
+        "icon": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIAAAACAAQMAAAD58POIAAAABlBMVEUAAAD4APit1uGJAAAAI0lEQVRIx2P4DwUMMDAqMCowKjAqQKTAaDCMCowKjAqQKQAABpD8LlM5SL4AAAAASUVORK5CYII",
+        "lastVersionId": "latest-release"
+    });
+    fs::write(&path, serde_json::to_string(&value)?)?;
+    Ok(())
 }
